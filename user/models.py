1a66b63b8960f90aa0897d23fb22e0db19ff297d
pooya.oa <pooya.oladazimi@tib.eu>
2024-11-19 12:52:48 +0100

    added user models methods

--- user/models.py
+++ user/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.conf import settings
-from typing import Optional
+from typing import Optional, Union
 from user_service.middlewares.request import get_client_id_from_request
 
 
-class Users(models.Model):
+class UserModel(models.Model):
     username = models.CharField()
     name = models.CharField(blank=True, null=True)
     created_at = models.DateTimeField()
@@ -18,6 +18,9 @@ class Users(models.Model):
     class Meta:
         db_table = "users"
         unique_together = (("username", "client_ts"),)
+
+    def __str__(self) -> str:
+        return f"<User {self.username}>"
 
     def to_dict(self) -> dict:
         return {
@@ -38,7 +41,7 @@ class Users(models.Model):
             return "invalid_auth_provider"
         if self.client_ts not in settings.CLIENT_TERMINOLOGY_SERVICES:
             return "invalid_client_ts"
-        user = Users.objects.filter(
+        user = UserModel.objects.filter(
             username=self.username, client_ts=self.client_ts
         ).first()
 
@@ -51,16 +54,103 @@ class Users(models.Model):
 
         return user
 
+    @staticmethod
     def get_by_username(username: str, client_ts: Optional[str] = None) -> dict:
         client = get_client_id_from_request() if not client_ts else client_ts
-        user = Users.objects.filter(username == username, client_ts == client).first()
+        user = UserModel.objects.filter(
+            username == username, client_ts == client
+        ).first()
         return user.to_dict()
 
+    @staticmethod
+    def get_by_id(user_id: Union[int, str], client_ts: Optional[str] = None) -> dict:
+        client = get_client_id_from_request() if not client_ts else client_ts
+        user = UserModel.objects.filter(id=user_id, client_ts=client).first()
+        return user.to_dict() if user else None
 
-class UserTokens(models.Model):
-    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    @staticmethod
+    def get_user_id_by_username(
+        username: str, client_ts: Optional[str] = None
+    ) -> Optional[int]:
+        client = get_client_id_from_request() if not client_ts else client_ts
+        db_user = UserModel.objects.filter(username=username, client_ts=client).first()
+        return db_user.id if db_user else None
+
+    @staticmethod
+    def get_user_name_by_id(
+        user_id: Union[int, str], client_ts: Optional[str] = None
+    ) -> Optional[str]:
+        client = get_client_id_from_request() if not client_ts else client_ts
+        db_user = UserModel.objects.filter(id=user_id, client_ts=client).first()
+        return db_user.username if db_user else None
+
+    @staticmethod
+    def block_user(username: str) -> bool:
+        db_user = UserModel.objects.filter(username=username).first()
+        if db_user:
+            db_user.is_blocked = True
+            db_user.save()
+            return True
+        return False
+
+    @staticmethod
+    def save_user_extra(
+        username: str, user_extra: dict, client_ts: Optional[str] = None
+    ) -> bool:
+        client = get_client_id_from_request() if not client_ts else client_ts
+        db_user = UserModel.objects.filter(username=username, client_ts=client).first()
+        if db_user:
+            db_user.user_extra = user_extra
+            db_user.save()
+            return True
+        return False
+
+    @staticmethod
+    def get_all_users() -> Optional[list]:
+        users = UserModel.objects.all()
+        return users
+
+
+class UserTokenModel(models.Model):
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     created_at = models.DateTimeField()
     token = models.CharField()
 
     class Meta:
         db_table = "user_tokens"
+
+    def __init__(
+        self,
+        user_id: Union[int, str, None] = None,
+        created_at: Optional[str] = None,
+        token: Optional[str] = None,
+    ) -> None:
+        self.user_id = user_id
+        self.token = token
+        self.created_at = created_at
+
+    def __str__(self) -> str:
+        return f"<UserToken {self.token}>"
+
+    def get_user_token_record(self) -> Optional[object]:
+        user_token_record = UserTokenModel.objects.filter(user == self.user_id).first()
+        return user_token_record
+
+    def get_user_token(self) -> Optional[str]:
+        user_token_record = UserTokenModel.objects.filter(user == self.user_id).first()
+        if user_token_record:
+            return user_token_record.token
+        return None
+
+    def register_token(self) -> bool:
+        user_token_record = UserTokenModel.objects.filter(id == self.id).first()
+        if not user_token_record:
+            self.save()
+        return True
+
+    def update_token(self, new_token: str) -> bool:
+        user_token_record = UserTokenModel.objects.filter(id == self.id).first()
+        if user_token_record:
+            user_token_record.token = new_token
+            user_token_record.save()
+        return True
