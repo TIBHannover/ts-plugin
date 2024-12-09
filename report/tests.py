b0802b42aa1973e5888a05e82d6856f5f1b9eb2d
pooya.oa <pooya.oladazimi@tib.eu>
2024-12-09 12:37:37 +0100

    added report app test and initiate test config

--- report/tests.py
+++ report/tests.py
@@ -1,3 +1,101 @@
 from django.test import TestCase
+from user.models import RoleModel, UserModel
+from note.models import NoteModel
+from datetime import datetime as _time
+from django.conf import settings
+from user_service.libs.test_config import BaseTest
+import copy
 
-# Create your tests here.
+
+class TestReport(TestCase, BaseTest):
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        
+        cls.report_form_data = {
+            "objectType": "note",
+            "objectId": 1,
+            "content": "Test content for report",
+            "ontology": "vibso"
+        }
+
+        cls.resolve_report_form_data = {
+            "objectType": "note",
+            "objectId": 1,
+            "action": "none",
+            "creatorUsername": "test-user"
+        }
+
+
+        user = UserModel.objects.filter(username=settings.test_github_username).first()
+        admin_role = RoleModel(
+            user=user,
+            created_at= _time.now(),
+            target_object_id= "system",
+            target_object_type= "system",
+            role= "admin",
+            client_ts= "general",
+            role_holder_email= "me@tib.eu"
+        )
+        admin_role.save()
+
+        note = NoteModel(
+            creator= user,
+            created_at= _time.now(),
+            ontology_id= cls.test_ontology_id,
+            content= "Test Content",
+            title="Test Note",
+            semantic_component_type= "class",
+            client_ts= "general",
+            semantic_component_iri= "some_iri",
+            semantic_component_label= "Test Label",
+            visibility= "public",
+            parent_ontology_id=cls.parent_ontology_id,
+        ) 
+        note.save()
+        cls.public_note_id = note.id
+
+
+        cls.report_form_data['objectId'] = note.id   
+        cls.resolve_report_form_data['objectId'] = note.id 
+        cls.report_create_url = cls.app_internal_base_url + '/report/create' 
+        cls.report_resolve_url = cls.app_internal_base_url + '/report/resolve'        
+
+
+    
+    def test_report_create_fail_for_guest_user(self):        
+        headers = copy.copy(self.guest_request_headers)        
+        response = self.client.post(self.report_create_url, headers=headers, data=self.report_form_data)                
+        self.assertEqual(response.status_code, 401)
+        self.assertIn("Not Authorized user", response.content.decode())
+    
+
+
+    def test_report_create_success_user(self):        
+        headers = copy.copy(self.orcid_request_headers)        
+        response = self.client.post(self.report_create_url, headers=headers, data=self.report_form_data)                        
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['_result']['report_created'], True)
+
+    
+
+
+    def test_report_resolve_fail_guest_user(self):        
+        headers = copy.copy(self.guest_request_headers)        
+        response = self.client.post(self.report_resolve_url, headers=headers, data=self.resolve_report_form_data)                        
+        self.assertEqual(response.status_code, 401)
+    
+
+
+    def test_report_resolve_fail_non_admin_user(self):        
+        headers = copy.copy(self.orcid_request_headers)        
+        response = self.client.post(self.report_resolve_url, headers=headers, data=self.resolve_report_form_data)                        
+        self.assertEqual(response.status_code, 401)
+    
+
+
+    def test_report_resolve_success_admin_user(self):        
+        headers = copy.copy(self.github_request_headers)        
+        response = self.client.post(self.report_resolve_url, headers=headers, data=self.resolve_report_form_data)                        
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['_result']['resolved'], True)
---
+++ user_service/libs/test_config.py
@@ -0,0 +1,52 @@
+from django.conf import settings
+
+class BaseTest:
+
+    test_ontology_id = "vibso"
+    test_parent_ontology_id = "bfo" 
+    app_internal_base_url = settings.APP_INTERNAL_BASE_URL                
+    github_access_token = settings.GITHUB_TEST_ACCESS_TOKE
+    orcid_access_token = settings.ORCID_TEST_ACCESS_TOKE
+    orcid_id = settings.ORCID_LOGIN_USERNAME
+    test_github_username = "github_" + settings.GITHUB_LOGIN_USERNAME
+    test_github_user_ts_token = settings.GITHUB_USER_TS_TOKEN
+    test_orcid_username = "orcid_" + settings.ORCID_LOGIN_USERNAME
+    test_orcid_user_ts_token = settings.ORCID_USER_TS_TOKEN
+    client_ts_id = "general"
+    client_ts_token = settings.FRONTEDN_AUTH_TOKEN
+    github_request_headers = {
+        "Authorization": github_access_token,
+        # "Content-Type": "application/json",
+        "X-TS-Auth-Provider": "github",             
+        "X-TS-Frontend-Id": client_ts_id,
+        "X-TS-Frontend-Token": client_ts_token,
+        "X-TS-User-Name": test_github_username,
+        "X-TS-User-Token": test_github_user_ts_token
+    }
+
+    orcid_request_headers = {
+        "Authorization": orcid_access_token, 
+        # "Content-Type": "application/json",                 
+        "X-TS-Orcid-Id": orcid_id,
+        "X-TS-Auth-Provider": "orcid",
+        "X-TS-Frontend-Id": client_ts_id,
+        "X-TS-Frontend-Token": client_ts_token,
+        "X-TS-User-Name": test_orcid_username,
+        "X-TS-User-Token": test_orcid_user_ts_token
+    }
+
+    
+    guest_request_headers = {
+        "Authorization": "",
+        # "Content-Type": "application/json",
+        "X-TS-Auth-Provider": "github",             
+        "X-TS-Frontend-Id": client_ts_id,
+        "X-TS-Frontend-Token": client_ts_token,
+        "X-TS-User-Name": test_github_username,
+        "X-TS-User-Token": "",            
+    }
+
+
+
+
+
