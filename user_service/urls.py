cfd16d4993e251a21c875152405a09ed94f7c441
pooya.oa <pooya.oladazimi@tib.eu>
2025-02-03 12:45:57 +0100

    disable admin panel

--- user_service/urls.py
+++ user_service/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin_panel/", admin.site.urls), 
+    # path("admin_panel/", admin.site.urls), 
     path("user/", include("user.urls")),
     path("report/", include("report.urls")),
     path("ontologysuggestion/", include("ontology_suggestion.urls")),
