1562ffde21106bbd465b6fbfe33a1aff0192bb67
pooya.oa <pooya.oladazimi@tib.eu>
2025-02-27 16:51:45 +0100

    export models in admin

--- note/admin.py
+++ note/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import NoteModel, NoteCommentModel
 
-# Register your models here.
+admin.site.register(NoteModel)
+admin.site.register(NoteCommentModel)
