1e17fcb74cca257e959e1c89b437b60edbf3e4e3
pooya <pooya.oladazimi@gmail.com>
2025-10-17 12:45:42 +0200

    update state and contact form
    include the client id in contact form labels.
    calculate ontology suggestion and contact form counts for stats.

--- admin/views.py
+++ admin/views.py
@@ -2,13 +2,13 @@ from user.models import UserModel, RoleModel
 from user.libs.auth import Auth
 from user_service.libs.decorators import (
     error_handler_decorator,
-    authentication_required
+    authentication_required,
 )
 from django.views.decorators.http import require_http_methods
 from user_service.middlewares.request import (
     get_headers_dict,
     get_username_from_request,
-    get_client_id_from_request
+    get_client_id_from_request,
 )
 import json
 from user_service.libs.utils import create_json_response
@@ -17,37 +17,46 @@ from report.models import ReportModel
 from github.models import GithubIssueRequestModel
 from django.conf import settings
 from django.http import Http404
-
+import urllib.parse
+import requests
 
 
 @error_handler_decorator
 @authentication_required
-@require_http_methods(['POST'])
+@require_http_methods(["POST"])
 def is_entity_admin(request):
     auth_object_dict = get_headers_dict()
     client_ts = get_client_id_from_request()
-    user = UserModel.objects.filter(username=get_username_from_request(), client_ts=client_ts).first()
-    auth_object_dict['user_id'] = user.id
-    auth_controller = Auth(**auth_object_dict)    
+    user = UserModel.objects.filter(
+        username=get_username_from_request(), client_ts=client_ts
+    ).first()
+    auth_object_dict["user_id"] = user.id
+    auth_controller = Auth(**auth_object_dict)
 
     _form = json.loads(request.body)
-    ontologyId = _form.get('ontologyId')
-    collectionId = _form.get('collectionId')
-    return create_json_response({'is_admin': auth_controller.is_user_admin_for_entity(ontologyId=ontologyId, collectionId=collectionId)})
-
-
+    ontologyId = _form.get("ontologyId")
+    collectionId = _form.get("collectionId")
+    return create_json_response(
+        {
+            "is_admin": auth_controller.is_user_admin_for_entity(
+                ontologyId=ontologyId, collectionId=collectionId
+            )
+        }
+    )
 
 
 @error_handler_decorator
 @authentication_required
-@require_http_methods(['POST'])
-def is_system_admin():    
+@require_http_methods(["POST"])
+def is_system_admin():
     client_ts = get_client_id_from_request()
-    user = UserModel.objects.filter(username=get_username_from_request(), client_ts=client_ts).first()
-    
+    user = UserModel.objects.filter(
+        username=get_username_from_request(), client_ts=client_ts
+    ).first()
+
     role_model = RoleModel.objects.filter(user=user, client_ts=client_ts).first()
-    is_admin = True if role_model.target_object_type == 'system' else False
-    return create_json_response({'is_system_admin': is_admin})
+    is_admin = True if role_model.target_object_type == "system" else False
+    return create_json_response({"is_system_admin": is_admin})
 
 
 @error_handler_decorator
@@ -56,10 +65,14 @@ def get_stats(request):
     header = get_headers_dict()
     if settings.STATS_API_TOKEN != header.get("access_token", "default"):
         raise Http404("not found")
-    
-    
+
+    headers = {
+        "PRIVATE-TOKEN": settings.GITLAB_TS_USER_API_TOKEN,
+        "Content-Type": "application/json",
+    }
+
     def calculate_stats(client_ts, stats):
-        users = UserModel.objects.filter(client_ts=client_ts).all();
+        users = UserModel.objects.filter(client_ts=client_ts).all()
         stats["user_count"] = len(users)
         note_count = 0
         comment_count = 0
@@ -74,7 +87,7 @@ def get_stats(request):
             comment_count += len(u.user_comments.all())
             collection_count += len(u.user_collections.all())
             termset_count += len(u.user_term_sets.all())
-            report_count += len(ReportModel.objects.filter(reporter = u).all()) 
+            report_count += len(ReportModel.objects.filter(reporter=u).all())
             github_issues = GithubIssueRequestModel.objects.filter(user=u).all()
             for gi in github_issues:
                 if gi.issue_type == "termRequest":
@@ -90,10 +103,40 @@ def get_stats(request):
         stats["github_issues_count"] = github_issues_count
         stats["term_request_count"] = term_request_count
 
-    client_ts = ["nfdi4chem", "general", "nfdi4ing"];
+    def calculate_gitlab_stats(client_ts, stats):
+        # ontology suggestion and contact form
+        url = settings.GITLAB_API_BASE_URL + "{}/issues".format(
+            urllib.parse.quote(settings.ONTOLOGY_SUGGESTION_REPO, safe="")
+        )
+
+        resp = requests.get(url, headers=headers)
+        ontology_suggestion_count = 0
+        for iss in resp.json():
+            if client_ts == "general" and "ontology_suggestion" in iss["labels"]:
+                ontology_suggestion_count += 1
+            elif (
+                "ontology_suggestion" in iss["labels"]
+                and client_ts.upper() in iss["labels"]
+            ):
+                ontology_suggestion_count += 1
+        stats["ontology_suggestion_count"] = ontology_suggestion_count
+
+        url = settings.GITLAB_API_BASE_URL + "{}/issues".format(
+            urllib.parse.quote(settings.CONTACT_REQUEST_RECEIVER_REPO, safe="")
+        )
+        resp = requests.get(url, headers=headers)
+        contact_form_count = 0
+        for iss in resp.json():
+            if client_ts in iss["labels"]:
+                contact_form_count += 1
+
+        stats["contact_form_count"] = contact_form_count
+
+    client_ts = ["nfdi4chem", "general", "nfdi4ing"]
     stats = {}
     for cts in client_ts:
         stats[cts] = {}
         calculate_stats(cts, stats[cts])
+        calculate_gitlab_stats(cts, stats[cts])
 
     return create_json_response({"stats": stats})
--- contact/views.py
+++ contact/views.py
@@ -9,13 +9,13 @@ from django.conf import settings
 import json
 
 
-@require_http_methods(['GET'])
+@require_http_methods(["GET"])
 def ping(request):
     return create_json_response({"response": "Pong"})
 
 
 @error_handler_decorator
-@require_http_methods(['POST'])
+@require_http_methods(["POST"])
 def create(request):
     data = json.loads(request.body)
     issueTypes = {"1": "Question", "2": "Problem"}
@@ -29,7 +29,9 @@ def create(request):
     safeAnswer = data["safeAnswer"]
 
     safeNumbers = safeQuestion.split("+")
-    if len(safeNumbers) != 2 or int(safeNumbers[0]) + int(safeNumbers[1]) != int(safeAnswer):
+    if len(safeNumbers) != 2 or int(safeNumbers[0]) + int(safeNumbers[1]) != int(
+        safeAnswer
+    ):
         return HttpResponseBadRequest("Invalid safe question and answer.")
 
     issue_content = "from: {} ({})\n\n{}".format(name, email, description)
@@ -37,7 +39,7 @@ def create(request):
     parameters = {
         "title": title,
         "description": issue_content,
-        "labels": issueTypes[type],
+        "labels": issueTypes[type] + "," + get_client_id_from_request(),
         "confidential": True,
     }
 
@@ -53,12 +55,12 @@ def create(request):
         "Content-Type": "application/json",
     }
     url = settings.GITLAB_API_BASE_URL + "{}/issues".format(
-        urllib.parse.quote(
-            settings.CONTACT_REQUEST_RECEIVER_REPO, safe=""
-        )
+        urllib.parse.quote(settings.CONTACT_REQUEST_RECEIVER_REPO, safe="")
     )
     response = requests.post(url, params=parameters, headers=headers)
     if response.status_code != 201:
-        return HttpResponseServerError("Failed to create issue. Please try again later.")
+        return HttpResponseServerError(
+            "Failed to create issue. Please try again later."
+        )
 
     return create_json_response({"response": "Issue created successfully"})
