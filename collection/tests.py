8efe53f5258472e2ba30864a8879ce84d7f68d24
pooya.oa <pooya.oladazimi@tib.eu>
2026-01-20 13:42:56 +0100

    update collection module tests

--- collection/tests.py
+++ collection/tests.py
@@ -5,13 +5,11 @@ from user_service.libs.test_helpers import TestHelper
 import json
 
 
-
 class TestCollection(TestCase, BaseTest):
-    
     @classmethod
     def setUpTestData(self) -> None:
-        self.github_user, _ = TestHelper.createGitHubUser()
-        TestHelper.createOrcidUser()
+        self.github_user = TestHelper.createGitHubUser()
+        self.orcidUser = TestHelper.createOrcidUser()
         self.collection = {
             "title": "Test creation",
             "content": "this is a test collection",
@@ -21,109 +19,132 @@ class TestCollection(TestCase, BaseTest):
             user=self.github_user,
             title="Test update",
             content="this is a test collection",
-            ontology_ids=["vibso"]
+            ontology_ids=["vibso"],
         ).to_dict()
-
-
-
+        self.github_user_jwt = TestHelper.generate_jwt(
+            {}, self.github_user.username, self.github_access_token
+        )
+        self.orcid_user_jwt = TestHelper.generate_jwt(
+            {},
+            self.orcidUser.username,
+            self.orcid_access_token,
+            self.orcid_id,
+        )
 
     def test_collection_creation_should_fail_for_guest(self):
         headers = copy.copy(self.guest_request_headers)
         url = "/collection/create/"
-        response = self.client.post(url, headers=headers, data=json.dumps(self.collection), content_type='application/json')
+        response = self.client.post(
+            url,
+            headers=headers,
+            data=json.dumps(self.collection),
+            content_type="application/json",
+        )
         self.assertEqual(response.status_code, 401)
-
-
 
     def test_collection_creation_should_fail_without_title(self):
         headers = copy.copy(self.github_request_headers)
         data = copy.copy(self.collection)
         data.pop("title")
         url = "/collection/create/"
-        response = self.client.post(url, headers=headers, data=json.dumps(data), content_type='application/json')
+        self.client.cookies["jwt"] = self.github_user_jwt
+        response = self.client.post(
+            url, headers=headers, data=json.dumps(data), content_type="application/json"
+        )
         self.assertEqual(response.status_code, 400)
         self.assertIn("Mandatory Fields are missing", response.content.decode())
-
-
 
     def test_collection_creation_should_success(self):
         headers = copy.copy(self.github_request_headers)
         url = "/collection/create/"
-        response = self.client.post(url, headers=headers, data=json.dumps(self.collection), content_type='application/json')
+        self.client.cookies["jwt"] = self.github_user_jwt
+        response = self.client.post(
+            url,
+            headers=headers,
+            data=json.dumps(self.collection),
+            content_type="application/json",
+        )
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()["_result"]["collection"]["title"], self.collection["title"])
-
-
+        self.assertEqual(
+            response.json()["_result"]["collection"]["title"], self.collection["title"]
+        )
 
     def test_get_collection_should_fail_for_non_owner_user(self):
         """
-            Collection is created by github user, but trying to get collection by orcid user
+        Collection is created by github user, but trying to get collection by orcid user
         """
         headers = copy.copy(self.orcid_request_headers)
-        url = "/collection/get/" + str(self.test_collection_for_db["id"]) + '/'
+        self.client.cookies["jwt"] = self.orcid_user_jwt
+        url = "/collection/get/" + str(self.test_collection_for_db["id"]) + "/"
         response = self.client.get(url, headers=headers)
         self.assertEqual(response.status_code, 404)
-
-
 
     def test_get_collection_should_fail_for_non_existing_id(self):
         headers = copy.copy(self.github_request_headers)
         url = "/collection/get/non_existing_id/"
+        self.client.cookies["jwt"] = self.github_user_jwt
         response = self.client.get(url, headers=headers)
         self.assertEqual(response.status_code, 404)
 
-        
-
     def test_get_collection_should_success(self):
         headers = copy.copy(self.github_request_headers)
-        url =  "/collection/get/" + str(self.test_collection_for_db["id"]) + '/'
+        url = "/collection/get/" + str(self.test_collection_for_db["id"]) + "/"
+        self.client.cookies["jwt"] = self.github_user_jwt
         response = self.client.get(url, headers=headers)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()["_result"]["collection"]["title"], self.test_collection_for_db["title"])
-
-
+        self.assertEqual(
+            response.json()["_result"]["collection"]["title"],
+            self.test_collection_for_db["title"],
+        )
 
     def test_get_collection_list_should_success(self):
         headers = copy.copy(self.github_request_headers)
         url = "/collection/get_list/"
+        self.client.cookies["jwt"] = self.github_user_jwt
         response = self.client.get(url, headers=headers)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.json()["_result"]["collections"]), 1)
 
-
-
     def test_collection_update_should_fail_for_non_owner_user(self):
         """
-            Collection is created by github user, but trying to update collection by orcid user
+        Collection is created by github user, but trying to update collection by orcid user
         """
         headers = copy.copy(self.orcid_request_headers)
-        url = "/collection/update/" + str(self.test_collection_for_db["id"]) + '/'
-        response = self.client.put(url, headers=headers, data=json.dumps(self.collection), content_type='application/json')
+        url = "/collection/update/" + str(self.test_collection_for_db["id"]) + "/"
+        self.client.cookies["jwt"] = self.orcid_user_jwt
+        response = self.client.put(
+            url,
+            headers=headers,
+            data=json.dumps(self.collection),
+            content_type="application/json",
+        )
         self.assertEqual(response.status_code, 404)
-
-
 
     def test_collection_update_should_success(self):
         headers = copy.copy(self.github_request_headers)
-        url = "/collection/update/" + str(self.test_collection_for_db["id"]) + '/'
-        response = self.client.put(url, headers=headers, data=json.dumps(self.collection), content_type='application/json')
+        url = "/collection/update/" + str(self.test_collection_for_db["id"]) + "/"
+        self.client.cookies["jwt"] = self.github_user_jwt
+        response = self.client.put(
+            url,
+            headers=headers,
+            data=json.dumps(self.collection),
+            content_type="application/json",
+        )
         self.assertEqual(response.status_code, 200)
-
-
 
     def test_collection_delete_should_fail_for_non_owner_user(self):
         """
-            Collection is created by github user, but trying to update collection by orcid user
+        Collection is created by github user, but trying to update collection by orcid user
         """
         headers = copy.copy(self.orcid_request_headers)
-        url = "/collection/delete/" + str(self.test_collection_for_db["id"]) + '/'
+        url = "/collection/delete/" + str(self.test_collection_for_db["id"]) + "/"
+        self.client.cookies["jwt"] = self.orcid_user_jwt
         response = self.client.delete(url, headers=headers)
         self.assertEqual(response.status_code, 404)
 
-
-
     def test_collection_delete_should_success(self):
         headers = copy.copy(self.github_request_headers)
-        url = "/collection/delete/" + str(self.test_collection_for_db["id"]) + '/'
+        url = "/collection/delete/" + str(self.test_collection_for_db["id"]) + "/"
+        self.client.cookies["jwt"] = self.github_user_jwt
         response = self.client.delete(url, headers=headers)
         self.assertEqual(response.status_code, 200)
