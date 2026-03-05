7b899bde2da604ebabad979964f93c0e188f4b8e
pooya <pooya.oladazimi@gmail.com>
2026-03-05 12:11:44 +0100

    add new services
    celery, celerybeat, and redis

--- user_service/settings.py
+++ user_service/settings.py
@@ -94,6 +94,7 @@ INSTALLED_APPS = [
     "admin_cli",
     "term_set",
     "ts",
+    "django_celery_beat",
 ]
 
 MIDDLEWARE = [
@@ -175,6 +176,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
 
 
 SUB_PATH = env("SUB_PATH", default="")
