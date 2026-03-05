aed10bc5b822a0b3b28bd80a8b3a48278fcc0ed0
pooya <pooya.oladazimi@gmail.com>
2026-03-05 17:23:39 +0100

    config celery

--- ts/urls.py
+++ ts/urls.py
@@ -4,9 +4,4 @@ from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path("ping/", views.ping, name="ping"),
-    path(
-        "harvest_datasets_terms_links/",
-        csrf_exempt(views.harvest_datasets_terms_links),
-        name="harvest_datasets_terms_links",
-    ),
 ]
