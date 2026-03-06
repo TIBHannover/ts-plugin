a4295133345b04a805052807756cf6f9b28562aa
pooya.oa <pooya.oladazimi@tib.eu>
2026-03-06 11:38:49 +0100

    extract doi source from doiRA

--- pub_link/urls.py
+++ pub_link/urls.py
@@ -4,4 +4,5 @@ from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path("ping/", csrf_exempt(views.ping), name="ping"),
+    path("create/", csrf_exempt(views.create_pub_link), name="create"),
 ]
