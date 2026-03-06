1245282828cdf0838165651fabe470c0967bbaba
pooya.oa <pooya.oladazimi@tib.eu>
2026-03-06 12:18:36 +0100

    refactoring

--- pub_link/views.py
+++ pub_link/views.py
@@ -22,25 +22,35 @@ def create_pub_link(request):
     doi = data["doi"]
     ontology_id = data["ontology_id"]
 
-    doi_id = ""
     doi = doi.strip()
-    if "https://dx.doi.org/" in doi:
-        doi_id = doi.split("https://dx.doi.org/")[1]
-    elif "https://doi.org/" in doi:
-        doi_id = doi.split("https://doi.org/")[1]
-    else:
-        doi_id = doi
+    doi_id = get_doi_id_from_url(doi)
+    doi_source = get_doi_source(doi_id)
 
+    return create_json_response({"response": doi_source})
+
+
+def get_doi_source(doi_id):
     doi_source_resp = requests.get("https://doi.org/doiRA/{}".format(doi_id))
     if doi_source_resp.status_code != 200:
-        return HttpResponseBadRequest("Invalid doi")
+        return ""
 
     doi_source = doi_source_resp.json()
     if len(doi_source) == 0:
-        return HttpResponseBadRequest("Invalid doi")
+        return ""
 
     doi_source = doi_source[0].get("RA")
     if doi_source is None:
-        return HttpResponseBadRequest("Invalid doi")
+        return ""
+    return doi_source
 
-    return create_json_response({"response": doi_source})
+
+def get_doi_id_from_url(url):
+    doi_id = ""
+    if "https://dx.doi.org/" in url:
+        doi_id = url.split("https://dx.doi.org/")[1]
+    elif "https://doi.org/" in url:
+        doi_id = url.split("https://doi.org/")[1]
+    else:
+        doi_id = url
+    return doi_id
+
