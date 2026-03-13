a5f708f51c73caa3d61c23ce8e9fb5aa7f1d0964
pooya <pooya.oladazimi@gmail.com>
2026-03-13 09:07:00 +0100

    implement pub link creation and pub link model

--- pub_link/models.py
+++ pub_link/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from user.models import UserModel
+from django.contrib.postgres.indexes import GinIndex
 
-# Create your models here.
+
+class PubLinkModel(models.Model):
+    creator = models.ForeignKey(UserModel, on_delete=models.DO_NOTHING)
+    doi = models.CharField()
+    citation = models.TextField()
+    ontology_id = models.CharField()
+    created_at = models.DateTimeField()
+
+    class Meta:
+        db_table = "ontology_pub_links"
+        indexes = [
+            GinIndex(
+                fields=["citation"], name="citation_index", opclasses=["gin_trgm_ops"]
+            ),
+        ]
+
+    def to_dict(self):
+        return {
+            "doi": self.doi,
+            "citation": self.citation,
+            "ontology_id": self.ontology_id,
+        }
+
+    def __str__(self):
+        return f"Ontology PubLink {self.ontology_id}:{self.doi}"
