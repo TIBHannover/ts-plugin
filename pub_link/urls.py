157fa9bb8007b23a87bfd59995ad0e06650165b7
pooya <pooya.oladazimi@gmail.com>
2026-03-13 10:17:32 +0100

    implement pub link get and delete

--- pub_link/urls.py
+++ pub_link/urls.py
@@ -5,4 +5,10 @@ from django.views.decorators.csrf import csrf_exempt
 urlpatterns = [
     path("ping/", csrf_exempt(views.ping), name="ping"),
     path("create/", csrf_exempt(views.create_pub_link), name="create"),
+    path(
+        "get/<str:ontology_id>/", csrf_exempt(views.get_pub_link), name="get_pub_links"
+    ),
+    path(
+        "delete/<int:id>/", csrf_exempt(views.delete_pub_link), name="delete_pub_link"
+    ),
 ]
