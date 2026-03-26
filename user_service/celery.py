1177d232777b64127a85cbeb143d1c060093785d
pooya <pooya.oladazimi@gmail.com>
2026-03-26 17:40:26 +0100

    implement storing term dataset links in the database

--- user_service/celery.py
+++ user_service/celery.py
@@ -4,5 +4,6 @@ from celery import Celery as CeleryApp
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "user_service.settings")
 
 app = CeleryApp("user_service")
+app.set_default()
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
